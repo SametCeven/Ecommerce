b08b925beb7bc60f9828e24d5e847da22696d279
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-05-25 16:19:53 +0300

    NavbarDark Component added

--- client/src/layout/PageContent.jsx
+++ client/src/layout/PageContent.jsx
@@ -2,11 +2,15 @@ import Carousel from "../components/Carousel";
 import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import NavbarDark from "./NavbarDark";
 
 export default function PageContent({ children }) {
 
     return (
-        <div className="py-5 flex flex-col gap-5">
+        <div className="flex flex-col gap-5">
+            <div className="hidden xl1440:block">
+                <NavbarDark></NavbarDark>
+            </div>
             <Header></Header>
             <Navbar></Navbar>
             <main>
